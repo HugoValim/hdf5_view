7b72f10aa644105743ab93d9a59f20dae7f0e4b3
Hugo Valim <hugo.campos@lnls.br>
2021-08-26 16:59:10 -0300

    Fixed the path to top dir

--- sol_view.py
+++ sol_view.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
 import os
+from os import path
 import subprocess
 
 # Call './motors_gui --help' for options
 # os.system("pydm --hide-nav-bar -m '{}, {}, {}, {}, {}, {}' ../gui/daf_gui.py &".format(mu,eta,chi,phi,nu,delta))
-subprocess.Popen("pydm --hide-nav-bar ./main.py", shell = True)
+top_dir = path.join(path.dirname(path.realpath(__file__)), 'main.py')
+print(top_dir)
+subprocess.Popen("pydm --hide-nav-bar '{}'".format(top_dir), shell = True)
